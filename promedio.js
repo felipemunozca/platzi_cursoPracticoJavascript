355fb58533f3a38227352b860290d41497fcfbed
Felipe Munoz <analista.felipe.m@gmail.com>
2021-11-26 00:36:56 -0300

    tercer taller, promedio completo

--- promedio.js
+++ promedio.js
@@ -29,3 +29,63 @@ function calcularMediaAritmetica(lista) {
 
 };
 
+/**
+ * 
+ * DESAFIO DEL CURSO
+ * 
+ */
+
+//funcion para filtrar, ordenar y devolver el arreglo 
+function calcularPromedio(listaPromedio) {
+    const listaContar = {};
+    
+    listaPromedio.map(
+        function (elemento) {
+            if (listaContar[elemento]){
+                listaContar[elemento] += 1;
+            }else {
+                listaContar[elemento] = 1;
+            }   
+        }
+    );
+
+    const sumaLista = listaPromedio.reduce(
+        function(valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    //const promedioLista = sumaLista / lista.length;
+    const promedioLista = sumaLista / listaPromedio.length;
+    
+    //const promedio = listaArreglo[listaArreglo.length - 1];
+    //return promedio[0];
+    return promedioLista;
+}
+
+//funcion para terminar el calculo e imprimir la respuesta del calculo en el formulario.
+function desafioCalcularPromedio() {
+
+    const entradaPromedio = document.getElementById("inputPromedio");
+    const promedioValores = entradaPromedio.value.split(',');
+    
+    for(let i = 0; i < promedioValores.length; i++){
+        promedioValores[i] = parseInt(promedioValores[i]);
+    }
+
+
+    
+    const resultado = calcularPromedio(promedioValores);
+    const mostrarResultado = document.getElementById("resultadoPromedio");
+    mostrarResultado.innerText = " " + resultado;
+
+}
+
+
+
+//funcion para limpiar la caja de texto
+function limpiarInput() {
+    document.getElementById("inputPromedio").value = "";
+    document.getElementsByClassName("resultados-numeros")[0].value = "";
+
+};
