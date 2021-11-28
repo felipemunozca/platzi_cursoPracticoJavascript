17222d701e2783b618d2da1bfd77b831c160ef7e
Felipe Munoz <analista.felipe.m@gmail.com>
2021-11-28 19:22:48 -0300

    tercer desafio completado

--- promedio.js
+++ promedio.js
@@ -55,11 +55,7 @@ function calcularPromedio(listaPromedio) {
         }
     );
 
-    //const promedioLista = sumaLista / lista.length;
-    const promedioLista = sumaLista / listaPromedio.length;
-    
-    //const promedio = listaArreglo[listaArreglo.length - 1];
-    //return promedio[0];
+    const promedioLista = sumaLista / listaPromedio.length;    
     return promedioLista;
 }
 
@@ -72,9 +68,7 @@ function desafioCalcularPromedio() {
     for(let i = 0; i < promedioValores.length; i++){
         promedioValores[i] = parseInt(promedioValores[i]);
     }
-
-
-    
+   
     const resultado = calcularPromedio(promedioValores);
     const mostrarResultado = document.getElementById("resultadoPromedio");
     mostrarResultado.innerText = " " + resultado;
@@ -82,10 +76,78 @@ function desafioCalcularPromedio() {
 }
 
 
-
 //funcion para limpiar la caja de texto
 function limpiarInput() {
     document.getElementById("inputPromedio").value = "";
     document.getElementsByClassName("resultados-numeros")[0].value = "";
+
+};
+
+
+/**
+ * 
+ * DESAFIO DEL PROMEDIO PONDERADO
+ * 
+ */
+
+
+function calcularPromedioNotas(){
+
+    //s1 = isNaN(parseInt(document.getElementById("s1").value)) ? 0 : parseInt(document.getElementById("s1").value);
+    const n1 = isNaN(parseFloat(document.getElementById("nota1").value)) ? 0 : parseFloat(document.getElementById("nota1").value);
+    const n2 = isNaN(parseFloat(document.getElementById("nota2").value)) ? 0 : parseFloat(document.getElementById("nota2").value);
+    const n3 = isNaN(parseFloat(document.getElementById("nota3").value)) ? 0 : parseFloat(document.getElementById("nota3").value);
+    const n4 = isNaN(parseFloat(document.getElementById("nota4").value)) ? 0 : parseFloat(document.getElementById("nota4").value);
+    const n5 = isNaN(parseFloat(document.getElementById("nota5").value)) ? 0 : parseFloat(document.getElementById("nota5").value);
+    const n6 = isNaN(parseFloat(document.getElementById("nota6").value)) ? 0 : parseFloat(document.getElementById("nota6").value);
+
+    const p1 = isNaN(parseInt(document.getElementById("ponderado1").value)) ? 0 : parseInt(document.getElementById("ponderado1").value);
+    const p2 = isNaN(parseInt(document.getElementById("ponderado2").value)) ? 0 : parseInt(document.getElementById("ponderado2").value);
+    const p3 = isNaN(parseInt(document.getElementById("ponderado3").value)) ? 0 : parseInt(document.getElementById("ponderado3").value);
+    const p4 = isNaN(parseInt(document.getElementById("ponderado4").value)) ? 0 : parseInt(document.getElementById("ponderado4").value);
+    const p5 = isNaN(parseInt(document.getElementById("ponderado5").value)) ? 0 : parseInt(document.getElementById("ponderado5").value);
+    const p6 = isNaN(parseInt(document.getElementById("ponderado6").value)) ? 0 : parseInt(document.getElementById("ponderado6").value);
+
+
+    const noMasDeCien = sumarPonderados(p1, p2, p3, p4, p5, p6);
+    
+    
+
+    if (noMasDeCien > 100) {
+        alert("Los porcentajes ponderados no pueden ser mayores al 100%");
+    } else {
+        const sacarProm = calcularNotasPromedio(n1, n2, n3, n4, n5, n6, p1, p2, p3, p4, p5, p6)
+        const resultadoPromPon = document.getElementById("resultadoPromedioPonderado");
+        resultadoPromPon.innerText = " " + sacarProm.toFixed(2);
+    }
+
+
+}
+
+function sumarPonderados(por1, por2, por3, por4, por5, por6){
+    const suma = (por1 + por2 + por3 + por4 + por5 + por6);
+    return suma
+}
+
+function calcularNotasPromedio(not1, not2, not3, not4, not5, not6, por1, por2, por3, por4, por5, por6){
+    const promedioNotas = ((not1*por1)+(not2*por2)+(not3*por3)+(not4*por4)+(not5*por5)+(not6*por6)) / (por1+por2+por3+por4+por5+por6);
+    //const promedioNotavalor1 + valor2 + valor3 + valor4 + valor5 + valor6s = ((nota1*valor1)+(nota2*valor2)) / (100);
+    return promedioNotas;
+}
+
+function reiniciarInput() {
+    document.getElementById("nota1").value = "";
+    document.getElementById("nota2").value = "";
+    document.getElementById("nota3").value = "";
+    document.getElementById("nota4").value = "";
+    document.getElementById("nota5").value = "";
+    document.getElementById("nota6").value = "";
+    document.getElementById("ponderado1").value = "";
+    document.getElementById("ponderado2").value = "";
+    document.getElementById("ponderado3").value = "";
+    document.getElementById("ponderado4").value = "";
+    document.getElementById("ponderado5").value = "";
+    document.getElementById("ponderado6").value = "";
+    document.getElementById("resultadoPromedioPonderado").value = "...";
 
 };
